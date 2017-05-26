10452b6fd3201763d13cfaaed6548dd9f0608413
sctyner <sctyner@iastate.edu>
2017-05-26 16:43:20 -0700

    delete old fn

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -7,35 +7,6 @@ library(httr)
 library(dplyr)
 library(JSOmetaN)
 
-summary.doc <- function(jsondata){
-  # number of documents
-  R <- nrow(jsondata)
-  # number of 1st degree keys
-  J <- ncol(jsondata)
-  
-  dummy_json <- matrix(NA,R,J)
-  docs.class <- rep(NA, J)
-  docs.size <- rep(NA, J)
-  for (j in 1:J){
-    docs.class[j] <- class(jsondata[,j])
-    docs.size[j] <- length(jsondata[,j])
-    for (i in 1:R){
-      # maybe add dim to this? 
-      # matrix -> something other than length below ?
-      dummy_json[i,j] <- sum(is.na(jsondata[i,j]))/length(jsondata[i,j])
-    }
-  }
-  
-  docs.per.key <- R - colSums(dummy_json)
-  
-  # add type of each key, total depth
-  tibble(Key = colnames(jsondata) , Doc.count = docs.per.key, 
-         Class = docs.class, Obj.len = docs.size)
-  
-  # vector to pass to summary.key
-  # invisible(jsondata)
-}
-
 ui <- dashboardPage(skin = "blue",
   dashboardHeader(title = "exploRing JSON"),
   dashboardSidebar(
